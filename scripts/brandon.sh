776f77fb410a5cb3b2a3d9073328009bc4d37fc1
pengzhenghao <pzh@cs.ucla.edu>
2024-05-12 18:00:13 -0700

    fix

--- scripts/brandon.sh
+++ scripts/brandon.sh
@@ -8,7 +8,7 @@ seeds=(0 100 200 300 400 500 600 700)
 for i in {0..3}
 do
     CUDA_VISIBLE_DEVICES=$i \
-    nohup python pvp/experiments/metadrive/train_td3cpl_metadrive_fakehuman.py \
+    nohup python pvp/experiments/metadrive/train_metadrive_multigoal.py \
     --seed=${seeds[$i]} \
     > "brandon-seed${seeds[$i]}.log" 2>&1 &
 done
