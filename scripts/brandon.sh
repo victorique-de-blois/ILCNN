d42750d346b62a6d1e651e86a9e423f1ccdfff04
pengzhenghao <pzh@cs.ucla.edu>
2024-05-16 15:19:26 -0700

    fix

--- scripts/brandon.sh
+++ scripts/brandon.sh
@@ -15,13 +15,13 @@ seeds=(0 100 200 300 400 500 600 700)
 
 # Let's say we want to grid search penalty = 0.5, 1, 2, 5, 10
 # Loop over it:
-CUDA_VISIBLE_DEVICES=7 \
-nohup python pvp/experiments/metadrive/train_metadrive_multigoal.py \
---wandb \
---seed=100 \
---lr=1e-4 \
---exp_name=td3_multigoal_0516 \
-> "brandon-td3.log" 2>&1 &
+#CUDA_VISIBLE_DEVICES=7 \
+#nohup python pvp/experiments/metadrive/train_metadrive_multigoal.py \
+#--wandb \
+#--seed=100 \
+#--lr=1e-4 \
+#--exp_name=td3_multigoal_0516 \
+#> "brandon-td3.log" 2>&1 &
 
 CUDA_VISIBLE_DEVICES=7 \
 nohup python pvp/experiments/metadrive/train_metadrive_multigoal_sac.py \
