9e0ded82cd0a8f880b478f976e987d49b6f0a2f4
pengzhenghao <pzh@cs.ucla.edu>
2024-06-27 19:49:58 -0700

    update brandon

--- scripts/brandon.sh
+++ scripts/brandon.sh
@@ -10,22 +10,6 @@ do
     CUDA_VISIBLE_DEVICES=$i \
     nohup python pvp/experiments/metadrive/train_metadrive_multigoal_sac.py \
     --seed=${seeds[$i]} \
+    --wandb \
     > "brandon-seed${seeds[$i]}.log" 2>&1 &
 done
-
-# Let's say we want to grid search penalty = 0.5, 1, 2, 5, 10
-# Loop over it:
-#CUDA_VISIBLE_DEVICES=7 \
-#nohup python pvp/experiments/metadrive/train_metadrive_multigoal.py \
-#--wandb \
-#--seed=100 \
-#--lr=1e-4 \
-#--exp_name=td3_multigoal_0516 \
-#> "brandon-td3.log" 2>&1 &
-
-CUDA_VISIBLE_DEVICES=7 \
-nohup python pvp/experiments/metadrive/train_metadrive_multigoal_sac.py \
---wandb \
---seed=100 \
---exp_name=sac_multigoal_0516 \
-> "brandon-sac.log" 2>&1 &
