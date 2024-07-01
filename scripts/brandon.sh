9361d9120f9631e152e5df9769cf834eeff8866e
pengzhenghao <pzh@cs.ucla.edu>
2024-07-01 09:24:51 -0700

    update TD3 script

--- scripts/brandon.sh
+++ scripts/brandon.sh
@@ -8,8 +8,9 @@ seeds=(0 100 200 300 400 500 600 700)
 for i in {0..3}
 do
     CUDA_VISIBLE_DEVICES=$i \
-    nohup python pvp/experiments/metadrive/train_metadrive_multigoal_sac.py \
+    nohup python pvp/experiments/metadrive/train_metadrive_multigoal_td3.py \
     --seed=${seeds[$i]} \
+    --exp_name=0701_td3_general \
     --wandb \
     > "brandon-seed${seeds[$i]}.log" 2>&1 &
 done
