0f8f2b6dd22d33a5d60d49f9ea373327a5a12331
pengzhenghao <pzh@cs.ucla.edu>
2024-07-06 15:23:25 -0700

    update script

--- scripts/brandon.sh
+++ scripts/brandon.sh
@@ -3,56 +3,56 @@
 # Define the seeds for each GPU
 seeds=(0 100 200 300 400 500 600 700)
 
+#for i in {0..1}
+#do
+#    CUDA_VISIBLE_DEVICES=$i \
+#    nohup python pvp/experiments/metadrive/train_ppo_metadrive.py \
+#    --seed=${seeds[$i]} \
+#    --exp_name="0706_ppo_regression" \
+#    --wandb \
+#    --wandb_projec="brandon" \
+#    --wandb_team="drivingforce" \
+#    > "brandon-seed${seeds[$i]}.log" 2>&1 &
+#done
+
+
 for i in {0..1}
 do
     CUDA_VISIBLE_DEVICES=$i \
-    nohup python pvp/experiments/metadrive/train_ppo_metadrive.py \
+    nohup python pvp/experiments/metadrive/train_metadrive_multigoal_ppo.py \
     --seed=${seeds[$i]} \
-    --exp_name="0706_ppo_regression" \
+    --exp_name="0706_ppo_traffic=0.1_lane_line_detector=0_NATIVE_REWARD" \
+    --traffic_density=0.1 \
     --wandb \
-    --wandb_projec="brandon" \
-    --wandb_team="drivingforce" \
+    --lane_line_detector=0 \
     > "brandon-seed${seeds[$i]}.log" 2>&1 &
 done
 
 
-#for i in {0..1}
-#do
-#    CUDA_VISIBLE_DEVICES=$i \
-#    nohup python pvp/experiments/metadrive/train_metadrive_multigoal_ppo.py \
-#    --seed=${seeds[$i]} \
-#    --exp_name="0706_ppo_traffic=0.1_lane_line_detector=0_NATIVE_REWARD" \
-#    --traffic_density=0.1 \
-#    --wandb \
-#    --lane_line_detector=0 \
-#    > "brandon-seed${seeds[$i]}.log" 2>&1 &
-#done
-#
-#
-#for i in {2..3}
-#do
-#    CUDA_VISIBLE_DEVICES=$i \
-#    nohup python pvp/experiments/metadrive/train_metadrive_multigoal_ppo.py \
-#    --seed=${seeds[$i]} \
-#    --exp_name="0706_ppo_traffic=0.0_lane_line_detector=0_NATIVE_REWARD" \
-#    --traffic_density=0.0 \
-#    --wandb \
-#    --lane_line_detector=0 \
-#    > "brandon-seed${seeds[$i]}.log" 2>&1 &
-#done
-#
-#
-#
-## Loop over each GPU
-#for i in {4..5}
-#do
-#    CUDA_VISIBLE_DEVICES=$i \
-#    nohup python pvp/experiments/metadrive/train_metadrive_multigoal_ppo.py \
-#    --seed=${seeds[$i]} \
-#    --exp_name="0706_ppo_traffic=0.1_lane_line_detector=0_NATIVE_REWARD" \
-#    --traffic_density=0.1 \
-#    --wandb \
-#    --lane_line_detector=0 \
-#    > "brandon-seed${seeds[$i]}.log" 2>&1 &
-#done
-#
+for i in {2..3}
+do
+    CUDA_VISIBLE_DEVICES=$i \
+    nohup python pvp/experiments/metadrive/train_metadrive_multigoal_ppo.py \
+    --seed=${seeds[$i]} \
+    --exp_name="0706_ppo_traffic=0.0_lane_line_detector=0_NATIVE_REWARD" \
+    --traffic_density=0.0 \
+    --wandb \
+    --lane_line_detector=0 \
+    > "brandon-seed${seeds[$i]}.log" 2>&1 &
+done
+
+
+
+# Loop over each GPU
+for i in {4..5}
+do
+    CUDA_VISIBLE_DEVICES=$i \
+    nohup python pvp/experiments/metadrive/train_metadrive_multigoal_ppo.py \
+    --seed=${seeds[$i]} \
+    --exp_name="0706_ppo_traffic=0.1_lane_line_detector=0_NATIVE_REWARD" \
+    --traffic_density=0.1 \
+    --wandb \
+    --lane_line_detector=0 \
+    > "brandon-seed${seeds[$i]}.log" 2>&1 &
+done
+
