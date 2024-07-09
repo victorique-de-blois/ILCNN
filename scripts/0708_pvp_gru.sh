97b143e80b4a15c95d47990f3bd3f3a6db06a802
pengzhenghao <pzh@cs.ucla.edu>
2024-07-09 16:24:19 -0700

    add script

---
+++ scripts/0708_pvp_gru.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Define the seeds for each GPU
+seeds=(0 100 200 300 400 500 600 700)
+
+
+
+
+
+# Loop over each GPU
+for i in {0..3}
+do
+    CUDA_VISIBLE_DEVICES=$i \
+    nohup python pvp/experiments/metadrive/train_pvp_metadrive_fakehuman_transformer.py \
+    --exp_name=0709_pvp_bcloss1_gru \
+    --bc_loss_weight=1.0 \
+    --wandb \
+    --wandb_project=pvp2024 \
+    --wandb_team=drivingforce \
+    --seed=${seeds[$i]} \
+    --free_level=0.95 \
+    --adaptive_batch_size=True \
+    --save_freq=10000 \
+    > "seed${seeds[$i]}.log" 2>&1 &
+done
+
