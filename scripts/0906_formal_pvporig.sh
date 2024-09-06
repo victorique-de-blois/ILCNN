c9081be461c302de96517f52dff1bd9682597af0
pengzhenghao <pzh@cs.ucla.edu>
2024-09-06 14:52:38 -0700

    Update 0906_formal_pvporig.sh

---
+++ scripts/0906_formal_pvporig.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Define the seeds for each GPU
+seeds=(0 100 200 300 400 500 600 700)
+
+
+# Loop over each GPU
+for i in {0..3}
+do
+    CUDA_VISIBLE_DEVICES=$i \
+    nohup python pvp/experiments/metadrive/train_pvp_metadrive_fakehuman.py \
+    --exp_name=0906_pvporig \
+    --bc_loss_weight=0.0 \
+    --adaptive_batch_size=False \
+    --wandb \
+    --wandb_project=pvp2024 \
+    --wandb_team=drivingforce \
+    --seed=${seeds[$i]} \
+    --free_level=0.95 \
+    --save_freq=10000 \
+    > "seed${seeds[$i]}.log" 2>&1 &
+done
+
+
+
+
+# Loop over each GPU
+for i in {4..7}
+do
+    CUDA_VISIBLE_DEVICES=$i \
+    nohup python pvp/experiments/metadrive/train_pvp_metadrive_fakehuman.py \
+    --exp_name=0906_pvporig_adap \
+    --bc_loss_weight=0.0 \
+    --adaptive_batch_size=True \
+    --wandb \
+    --wandb_project=pvp2024 \
+    --wandb_team=drivingforce \
+    --seed=${seeds[$i]} \
+    --free_level=0.95 \
+    --save_freq=10000 \
+    > "seed${seeds[$i]}.log" 2>&1 &
+done
+
