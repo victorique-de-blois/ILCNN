ba4c47852a7e5bf40ca3128d8d79d8a3224f0122
pengzhenghao <pzh@cs.ucla.edu>
2024-09-08 14:45:56 -0700

    Up

--- scripts/0906_formal_pvporig.sh
+++ scripts/0906_formal_pvporig.sh
@@ -9,14 +9,14 @@ for i in {0..7}
 do
     CUDA_VISIBLE_DEVICES=$i \
     nohup python pvp/experiments/metadrive/train_pvp_metadrive_fakehuman.py \
-    --exp_name=0906_pvporig_adap \
-    --bc_loss_weight=0.0 \
-    --adaptive_batch_size=True \
+    --exp_name=0908_pvporig_noadap_bcloss \
+    --bc_loss_weight=1.0 \
+    --adaptive_batch_size=False \
     --wandb \
     --wandb_project=pvp2024 \
     --wandb_team=drivingforce \
     --seed=${seeds[$i]} \
     --free_level=0.95 \
     --save_freq=10000 \
-    > "seed${seeds[$i]}_adap.log" 2>&1 &
+    > "seed${seeds[$i]}_0908_pvporig_noadap_bcloss.log" 2>&1 &
 done
